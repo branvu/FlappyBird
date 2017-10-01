c3b9ee52c6ab869045db25f939478addb22a04fd
Brandon Vu <brangogohi@gmail.com>
2017-10-01 16:45:57 -0400

    Added JUnit and a test class

---
+++ core/src/test/JavaExercises.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.junit.runner.RunWith;
+
+/**
+ * You will complete your java exercises here
+ */
+@RunWith(org.mockito.junit.MockitoJUnitRunner.class)
+public class JavaExercises {
+    
+}
