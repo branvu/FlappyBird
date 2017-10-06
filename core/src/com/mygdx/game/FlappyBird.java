444e6ff39697140f58c9616413aa4a8dba53ab67
Brandon Vu <brangogohi@gmail.com>
2017-10-05 22:13:34 -0400

    Added variables to make it work

--- core/src/com/mygdx/game/FlappyBird.java
+++ core/src/com/mygdx/game/FlappyBird.java
@@ -3,7 +3,9 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 
 public class FlappyBird extends ApplicationAdapter {
-
+	public static int WIDTH = 0;
+	public static int HEIGHT = 0;
+	public static String TITLE = "String";
 	@Override
 	public void create () {
 	}
