fa54d2aabb427e2e498cebc63562d1af07ff2229
Brandon Vu <brangogohi@gmail.com>
2017-10-29 20:14:47 -0400

    Java excerise example

--- core/src/exercises/JavaExercises.java
+++ core/src/exercises/JavaExercises.java
@@ -10,17 +10,76 @@ import org.junit.runner.RunWith;
 public class JavaExercises {
 
     @Test public void ReplaceThisWithYourMethod(){
+    }
+
+
+    @Test public void doSomething(){
 
     }
 
-    @Test public void emptyMethod(){
+    public void add(){
+
+    }
+    public int something(){
+        return 1 + 2;
+    }
+    public double something2(double variable, int variable2){
+        return variable + variable2;
+    }
+    public boolean isTrue(double number){
+        if(number > 1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public void printName(String name){
+        System.out.println("Your name is " + name);
+    }
+    @Test public void callMethod(){
+        System.out.println(isTrue(2.1));
+        Library LansdaleLib = new Library("book", "red",1,1,0,150,"Lansdale",false,70);
+        LansdaleLib.checkInBook("book");
+        LansdaleLib.classbookName = "7";
+        System.out.println(LansdaleLib.classbookName);
+    }
+
+
+public class Library{
+    String classbookName;
+    String color;
+    int shelves;
+    int walls;
+    int ceilings;
+    int computers;
+    String name;
+    boolean hasAir;
+    public Library(String bookNameInput, String color, int shelves, int walls, int
+                   ceilings, int computers, String name, boolean hasAir, int numberOfBaths){
+        classbookName = bookNameInput;
+        this.ceilings = ceilings;
+        this.color = color;
+        this.walls = walls;
+        this.computers = computers;
+        this.name = name;
+        this.shelves = shelves;
+        this.name = name;
+        this.hasAir = hasAir;
 
     }
 
-    @Test public void method(double t, double height, double width){
-        System.out.print("X cordinate: " + (500 - t - 40 - width) + "Y coordinate: " + 100);
-    }
+        public void checkInBook(String bookName){
+            //Checked in book
+            this.classbookName = bookName;
+        }
+        public String checkOutBook(){
+            return classbookName;
+        }
 
 }
+
+}
+
 
 
