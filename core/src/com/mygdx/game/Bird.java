176a80bb43f990910eaeeda1e8418fafe24c156c
Brandon Vu <brangogohi@gmail.com>
2017-11-08 19:13:25 -0500

    Changed Vector3 to Vector2

--- core/src/com/mygdx/game/Bird.java
+++ core/src/com/mygdx/game/Bird.java
@@ -3,6 +3,7 @@ package com.mygdx.game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 
 /**
@@ -13,25 +14,25 @@ import com.badlogic.gdx.math.Vector3;
 
 public class Bird {
     private static final int GRAVITY = -15;
-    private Vector3 position;//Holds x,y, and z
-    private Vector3 velocity;
+    private Vector2 position;//Holds x,y, and z
+    private Vector2 velocity;
     private Texture bird;
     private Rectangle bounds;
     private Texture texture;
     private static final int MOVEMENT  = 100;
     public Bird(int x, int y){
-        position = new Vector3(x,y,0);
-        velocity = new Vector3(0,0,0);//Starts idle
+        position = new Vector2(x,y);
+        velocity = new Vector2(0,0);//Starts idle
         bird = new Texture("bird(3).png");
         bounds = new Rectangle(x,y,bird.getWidth(),bird.getHeight());//A rectangle around the bird to detect collisions
 
     }
     public void update(float dt){
         if(position.y > 0)//If the bird is above 0, gravity applies
-            velocity.add(0,GRAVITY,0);//Add the gravity to the bird every update
+            velocity.add(0,GRAVITY);//Add the gravity to the bird every update
         //velocity.scl(dt);//Multiply everything by delta time, Scales with time (Put everything in delta time so that we can add the movement also scaled in delta time
 
-        position.add(MOVEMENT*dt,velocity.y*dt,0);//
+        position.add(MOVEMENT*dt,velocity.y*dt);//
 
         if(position.y < 0){//The bird cannot go lower than the ground
             position.y = 0;
@@ -45,7 +46,7 @@ public class Bird {
         return bounds;
     }
 
-    public Vector3 getPosition() {
+    public Vector2 getPosition() {
         return position;
     }
 
