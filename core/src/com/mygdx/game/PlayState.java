c2d19a5a8e114ae5a2da2c575461878d3c0526ca
Brandon Vu <brangogohi@gmail.com>
2017-11-15 21:27:08 -0500

    Code added

--- core/src/com/mygdx/game/PlayState.java
+++ core/src/com/mygdx/game/PlayState.java
@@ -20,7 +20,10 @@ public class PlayState {
     protected OrthographicCamera cam;//Camera
     protected Vector3 mouse;//Mouse
 
+    protected Bird bird;
+
     public PlayState() {
+        bird = new Bird(45,10);
         cam = new OrthographicCamera();
         mouse = new Vector3();
         cam.setToOrtho(false, FlappyBird.WIDTH / 2, FlappyBird.HEIGHT / 2);//Sets the camera's origin lower left
@@ -29,7 +32,16 @@ public class PlayState {
 
     }
 
+    protected void handleInput() {
+        if(Gdx.input.justTouched()){
+            bird.jump();
+        }
+    }
+
     public PlayState update(float dt) {
+        handleInput();
+        cam.position.x = bird.getPosition().x + 80;//Center the camera on the bird
+        bird.update(dt);
         cam.update();//Tells to the camera that we repositioned
         return this;
     }
@@ -38,6 +50,7 @@ public class PlayState {
         sb.setProjectionMatrix(cam.combined);//The coordinate game screen starts bottom left
         sb.begin();
         sb.draw(background,cam.position.x - (cam.viewportWidth/2),0);//Viewport is centered in middle of screen, so draw it halfway off the center
+        sb.draw(bird.getBird(),bird.getPosition().x,bird.getPosition().y);//Draws the bird in the position
 
 
         sb.end();//Close the sprite batch
