10dc435629b8a2ced46c9f335a0f9f1f0c10b061
Brandon Vu <brangogohi@gmail.com>
2017-12-07 16:39:00 -0500

    Got rid of tube array and replaced with four Tube objects

--- core/src/com/mygdx/game/PlayState.java
+++ core/src/com/mygdx/game/PlayState.java
@@ -15,21 +15,19 @@ import com.badlogic.gdx.utils.Array;
 
 public class PlayState {
     private Bird bird;
-    private Tube tube;
     private final static int GROUND_Y_OFFSET = -50;//Offset to place the ground correctly
     private Texture ground;
     private static final int TUBE_SPACING = 100;//Spacing between tubes
     private static final int TUBE_COUNT = 4;
     private Texture background;
-    private Array<Tube> tubes;//Array of tubes
     private Vector2 groundPos1, groundPos2;
     protected OrthographicCamera cam;//Camera
     protected Vector2 mouse;//Mouse
-
+    private Tube tube1, tube2, tube3, tube4;
+    private boolean gameOver = false;
     public PlayState() {
         ground = new Texture("ground.jpg");
         bird = new Bird(50,300);
-        tube = new Tube(200);
         cam = new OrthographicCamera();
         mouse = new Vector2();
         cam.setToOrtho(false, FlappyBird.WIDTH / 2, FlappyBird.HEIGHT / 2);//Sets the camera's origin lower left
@@ -37,10 +35,10 @@ public class PlayState {
         groundPos1 = new Vector2(cam.position.x - (cam.viewportWidth / 2),GROUND_Y_OFFSET);
         groundPos2 = new Vector2((cam.position.x - (cam.viewportWidth / 2)) + ground.getWidth(),GROUND_Y_OFFSET);
         background = new Texture("bgPlay.png");
-        tubes = new Array<Tube>();//Initialize the tubes array
-        for(int i = 1; i <= TUBE_COUNT;i++){
-            tubes.add(new Tube(i * (TUBE_SPACING + Tube.TUBE_WIDTH)));//Adding the tubes to the screen at different x cordinates
-        }
+        tube1 = new Tube((TUBE_SPACING + Tube.TUBE_WIDTH));//Adding the tubes to the screen at different x cordinates
+        tube2 = new Tube((TUBE_SPACING + Tube.TUBE_WIDTH) * 2);
+        tube3 = new Tube((TUBE_SPACING + Tube.TUBE_WIDTH) * 3);
+        tube4 = new Tube((TUBE_SPACING + Tube.TUBE_WIDTH) * 4);
     }
 
     protected void handleInput() {
@@ -54,15 +52,32 @@ public class PlayState {
         updateGround();
         bird.update(dt);
         cam.position.x = bird.getPosition().x + 80;//Center the camera on the bird
-        for(int i = 0; i <tubes.size; i++){//Do this rather than below because other way will throw nested error
-            Tube tube = tubes.get(i);
-            if(cam.position.x - (cam.viewportWidth / 2) > tube.getPosTopTube().x + tube.getTopTube().getWidth())
-                //If the camera's left origin is greater than the right end of the tube's X POSITION, reposition the tube
-                tube.reposition(tube.getPosTopTube().x + ((Tube.TUBE_WIDTH + TUBE_SPACING) * TUBE_COUNT));//Evenly puts the next Top tube
-            if(tube.collides(bird.getBounds())){
-                dispose();
-                return new PlayState();
+        for(int i = 0; i < 4; i++){//Do this rather than below because other way will throw nested error
+            if(i == 0){
+                updateTube(tube1);
+                if(gameOver){
+                    return new PlayState();
+                }
             }
+            else if (i == 1){
+                updateTube(tube2);
+                if(gameOver){
+                    return new PlayState();
+                }
+            }
+            else if (i == 2){
+                updateTube(tube3);
+                if(gameOver){
+                    return new PlayState();
+                }
+            }
+            else if (i == 3){
+                updateTube(tube4);
+                if(gameOver){
+                    return new PlayState();
+                }
+            }
+
         }
         if(bird.getPosition().y <= ground.getHeight() + GROUND_Y_OFFSET){//If bird touches the ground
             dispose();
@@ -77,10 +92,10 @@ public class PlayState {
         sb.begin();
         sb.draw(background,cam.position.x - (cam.viewportWidth/2),0);//Viewport is centered in middle of screen, so draw it halfway off the center
         sb.draw(bird.getBird(),bird.getPosition().x,bird.getPosition().y);//Draws the bird in the position
-        for(Tube tube : tubes){//For every tube in the tube array........
-            sb.draw(tube.getTopTube(),tube.getPosTopTube().x,tube.getPosTopTube().y);//Draw the top tube
-            sb.draw(tube.getBotTube(),tube.getPosBotTube().x,tube.getPosBotTube().y);//Draw the bottom tube
-        }
+        renderTube(tube1,sb);
+        renderTube(tube2,sb);
+        renderTube(tube3,sb);
+        renderTube(tube4,sb);
         sb.draw(ground,groundPos1.x,groundPos1.y);//Draw the first ground
         sb.draw(ground,groundPos2.x,groundPos2.y);//Draw the second
         sb.end();//Close the sprite batch
@@ -90,9 +105,10 @@ public class PlayState {
         background.dispose();
         bird.dispose();
         ground.dispose();
-        for(Tube tube : tubes){//Dispose every tube in the tubes array
-            tube.dispose();
-        }
+        tube1.dispose();
+        tube2.dispose();
+        tube3.dispose();
+        tube4.dispose();
     }
     private void updateGround(){
         if(cam.position.x - (cam.viewportWidth /2) > groundPos1.x + ground.getWidth()){//If the camera position is in front of the first ground,...
@@ -101,5 +117,21 @@ public class PlayState {
         if(cam.position.x - (cam.viewportWidth /2) > groundPos2.x + ground.getWidth()){
             groundPos2.add(ground.getWidth() *2,0);
         }
+    }
+    private void updateTube(Tube tube){
+        if(cam.position.x - (cam.viewportWidth / 2) > tube.getPosTopTube().x + tube.getTopTube().getWidth())
+            //If the camera's left origin is greater than the right end of the tube's X POSITION, reposition the tube
+            tube.reposition(tube.getPosTopTube().x + ((Tube.TUBE_WIDTH + TUBE_SPACING) * TUBE_COUNT));//Evenly puts the next Top tube
+        if(tube.collides(bird.getBounds())){
+            dispose();
+            gameOver = true;
+        }
+        else{
+            gameOver = false;
+        }
+    }
+    private void renderTube(Tube tube, SpriteBatch sb){
+        sb.draw(tube.getTopTube(),tube.getPosTopTube().x,tube.getPosTopTube().y);//Draw the top tube
+        sb.draw(tube.getBotTube(),tube.getPosBotTube().x,tube.getPosBotTube().y);//Draw the bottom tube
     }
 }
